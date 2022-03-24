3d04bc3b7a9611fc69aff01a2a1c328b66700e7c
Afeez G Lawal <afeezlg@gmail.com>
2022-03-24 15:50:36 +0100

    first commit

---
+++ src/components/Layout.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Layout = () => {
+  return (
+    <>
+      <nav
+        role={"navigation"}
+        aria-roledescription="Primary"
+        aria-label="Primary"
+      ></nav>
+      <main></main>
+    </>
+  );
+};
+
+export default Layout;
