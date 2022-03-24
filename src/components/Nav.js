dc2bd520365cf8e49695dcd131f246e46b9ac3ae
Afeez G Lawal <afeezlg@gmail.com>
2022-03-24 18:11:08 +0100

    added gelion font

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav
+      role={"navigation"}
+      aria-roledescription="Primary"
+      aria-label="Primary"
+    >
+      <p>Gelion</p>
+    </nav>
+  );
+};
+
+export default Nav;
