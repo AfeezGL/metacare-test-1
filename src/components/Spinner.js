59e6c8ce39bce5c28a9cb7a9dd7ae2e5dd7adedc
Afeez G Lawal <afeezlg@gmail.com>
2022-04-05 22:26:02 +0100

    resized spinner container

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,7 +2,7 @@ const Spinner = () => {
   return (
     <div
       className="d-flex justify-content-center align-items-center"
-      style={{ height: "700px" }}
+      style={{ height: "300px" }}
     >
       <div className="spinner-border text-info" role="status">
         <span className="visually-hidden">Loading...</span>
