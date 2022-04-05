c7148ada8c02855660068035c5f49e0327d7afa2
Afeez G Lawal <afeezlg@gmail.com>
2022-04-05 11:04:20 +0100

    added layout component and implemented routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import PrimaryNav from "./components/PrimaryNav";
-import Teams from "./pages/Teams";
 import { BrowserRouter } from "react-router-dom";
-import MainHeader from "./components/MainHeader";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <BrowserRouter>
       <PrimaryNav />
-      <div className="display">
-        <MainHeader />
-        {/* this is the main container for all pages */}
-        <>
-          <Teams />
-        </>
-      </div>
+      <Layout />
     </BrowserRouter>
   );
 }
---
+++ src/components/Layout.js
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Teams from "../pages/Teams";
+import MainHeader from "./MainHeader";
+
+const Layout = () => {
+  return (
+    <div className="display">
+      <MainHeader />
+      {/* this is the main container for all pages */}
+      <Routes>
+        <Route path="/" element={<Teams />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default Layout;
