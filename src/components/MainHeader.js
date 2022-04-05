120be324b6ee0a08a3ebbec54f682aae44398387
Afeez G Lawal <afeezlg@gmail.com>
2022-04-05 18:07:28 +0100

    completed user section in the page header

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,5 +1,7 @@
 import { ReactComponent as SearchIcon } from "../icons/search.svg";
 import { ReactComponent as BellIcon } from "../icons/bell.svg";
+import { ReactComponent as User } from "../icons/user.svg";
+import { Link } from "react-router-dom";
 
 const MainHeader = () => {
   return (
@@ -17,9 +19,34 @@ const MainHeader = () => {
       </form>
 
       <div className="d-flex align-items-center border border-1 rounded pt-2 pb-2 ps-3 pe-3">
-        <div className="notification d-inline-flex">
+        <div className="[ notification ] d-inline-flex">
           <BellIcon />
-          <span className="badge bg-danger">3</span>
+          <span className="[ notification-badge ]">3</span>
+        </div>
+        <div className="[ user ]">
+          <div
+            className="d-flex align-items-center justify-content-between"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#user"
+            aria-expanded="false"
+            aria-controls="admin"
+          >
+            <span>
+              <User />
+            </span>
+          </div>
+
+          <div className="collapse" id="user">
+            <ul>
+              <li>
+                <Link to={"profile"}>Profile</Link>
+              </li>
+              <li>
+                <Link to={"settings"}>Settings</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </header>
